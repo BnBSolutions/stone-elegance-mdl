18df3b1d143f9de3bb520c6227199b6c0e56daa8
BnbSolutions <vaniocvanioc91@gmail.com>
2026-02-03 12:51:33 +0000

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/stone-elegance-mdl/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/produse" element={<Produse />} />
