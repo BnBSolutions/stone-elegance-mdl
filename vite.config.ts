c64b0dfcbc58a3021ca7742b1e364f428c6406d0
BnbSolutions <vaniocvanioc91@gmail.com>
2026-02-03 12:48:01 +0000

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/stone-elegance-mdl/',
   server: {
     host: "::",
     port: 8080,
